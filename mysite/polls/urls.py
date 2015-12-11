a1734403ae79ffa074e7d1a316e81ef51979cc44
yell0wc0w <dark_shadex@hotmail.com>
2015-12-11 09:34:37 -0500

    Trying to connectDB to HTML, almost there...

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,11 +5,11 @@ app_name = 'polls'
 
 urlpatterns = [
     # ex: /cfbwebapp/
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.AthleteView.as_view(), name='index'),
     # ex: /cfbwebapp/5/
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # ex: /cfbwebapp/5/results/
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # ex: /cfbwebapp/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # # ex: /cfbwebapp/5/results/
+    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    # # ex: /cfbwebapp/5/vote/
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
