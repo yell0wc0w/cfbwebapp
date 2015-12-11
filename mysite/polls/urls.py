2226912643c189d26e2d31b9416817662a555524
yell0wc0w <dark_shadex@hotmail.com>
2015-12-11 12:03:31 -0500

    Search text bar now working!

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,8 +8,5 @@ urlpatterns = [
     url(r'^$', views.AthleteView, name='index'),
     # ex: /cfbwebapp/5/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # # ex: /cfbwebapp/5/results/
-    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # # ex: /cfbwebapp/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
 ]
