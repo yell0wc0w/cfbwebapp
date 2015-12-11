7a2b223737f0ecf555d8a4c008a70039a4a7690f
yell0wc0w <dark_shadex@hotmail.com>
2015-12-11 07:06:59 -0500

    changed URL to cfbwebapp

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,14 +4,12 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    # ex: /polls/
+    # ex: /cfbwebapp/
     url(r'^$', views.IndexView.as_view(), name='index'),
-    # ex: /polls/5/
+    # ex: /cfbwebapp/5/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # ex: /polls/5/results/
+    # ex: /cfbwebapp/5/results/
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # ex: /polls/5/vote/
+    # ex: /cfbwebapp/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-    # ex: /polls/
-    url(r'^$', views.IndexView.as_view(), name='index'),
 ]
