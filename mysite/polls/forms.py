ffd53f80e6f188fb6e8e8cf3730352b7f517a695
yell0wc0w <dark_shadex@hotmail.com>
2015-12-22 07:02:49 -0500

    Now able to save in DB from inline editor yay!!

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class AthleteNameForm(forms.Form):
-    athleteName = forms.CharField(label='Search for athlete', max_length=100)
+    athletename = forms.CharField(label='Search for athlete', max_length=100)
